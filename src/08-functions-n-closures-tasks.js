426dc282dad94968ab342ccfe811564acb0a4958
Daniyar <danikman89@gmail.com>
2022-07-26 01:39:20 +0600

    feat: add solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -77,8 +77,9 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const res = func();
+  return () => res;
 }
 
 /**
@@ -161,8 +162,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let startNum = startFrom - 1;
+  return () => {
+    startNum += 1;
+    return startNum;
+  };
 }
 
 module.exports = {
